4cc89f3b0923fa17cd3b4c22d2142112291552c6
Raflirr70 <rafliradiansyah70@gmail.com>
2024-10-06 16:02:32 +0700

    Add : folder untuk melakukan kerja kelompok, lengkap dengan tailwind

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../Public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,  // Agar container otomatis berada di tengah
+      padding: '2rem',  // Menambahkan padding
+      screens: {
+        sm: '100%',   // Lebar container pada layar kecil
+        md: '768px',  // Lebar container pada layar medium
+        lg: '1024px', // Lebar container pada layar besar
+        xl: '1280px', // Lebar container pada layar sangat besar
+      },
+    },
+  },
+  plugins: [],
+}
+
