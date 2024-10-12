684833a97f69f90623343ee7a7cc1881f6947f42
Raflirr70 <rafliradiansyah70@gmail.com>
2024-10-12 10:34:36 +0700

    Edit : Membuat dasaran Konten di Main

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -5,13 +5,13 @@ module.exports = {
     extend: {},
     container: {
       center: true,  // Agar container otomatis berada di tengah
-      padding: '2rem',  // Menambahkan padding
-      screens: {
-        sm: '100%',   // Lebar container pada layar kecil
-        md: '768px',  // Lebar container pada layar medium
-        lg: '1024px', // Lebar container pada layar besar
-        xl: '1280px', // Lebar container pada layar sangat besar
-      },
+      // padding: '2rem',  // Menambahkan padding
+      // screens: {
+      //   sm: '100%',   // Lebar container pada layar kecil
+      //   md: '768px',  // Lebar container pada layar medium
+      //   lg: '1024px', // Lebar container pada layar besar
+      //   xl: '1280px', // Lebar container pada layar sangat besar
+      // },
     },
   },
   plugins: [],
